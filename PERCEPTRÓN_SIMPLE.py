77466069aca281314ac6d517756b8ec79b984d9b
SLaxum <55930519+SLaxum@users.noreply.github.com>
2021-02-16 12:32:23 -0500

    Update PERCEPTRÓN_SIMPLE.py

---
+++ PERCEPTRÓN_SIMPLE.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+#MATRICES
+
+#ENTRADAS
+X = np.array([[1,0,1], [0,1,1], [1,1,0]])
+
+#SALIDAS
+YD = np.array([[1,0], [0,1], [1,1]])
+
+#CONFIGURACION DE LA NEURONA
+M = 3
+N = 2
+P = 3
+
+RA = 1
+ER = 0.1
+NI = 1000
+
+#APRENDIZAJE
+for SALIDAS
+
+
+print("X = ", X)
+print("")
+print("Y = ", YD)
