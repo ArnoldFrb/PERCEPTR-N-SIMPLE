6a2220e745c1b73e91e43c1a2e9849ae16afd837
SLaxum <55930519+SLaxum@users.noreply.github.com>
2021-02-16 13:04:40 -0500

    Update PERCEPTRÓN_SIMPLE.py

--- PERCEPTRÓN_SIMPLE.py
+++ PERCEPTRÓN_SIMPLE.py
@@ -8,6 +8,12 @@ X = np.array([[1,0,1], [0,1,1], [1,1,0]])
 #SALIDAS
 YD = np.array([[1,0], [0,1], [1,1]])
 
+#PESOS
+PESOS = np.array([[0.1,-0.5,-0.9], [0.6,0.2,-0.3]])
+
+#UMBRAL
+UMBRAL = np.array([[0.5], [-0.8]]) 
+
 #CONFIGURACION DE LA NEURONA
 M = 3
 N = 2
