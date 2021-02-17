3304c566a6323378e021f7d57013caf0a85747df
luisfersk <39956205+luisfersk@users.noreply.github.com>
2021-02-17 15:46:00 -0500

    ERROR

--- PERCEPTRÓN_SIMPLE.py
+++ PERCEPTRÓN_SIMPLE.py
@@ -1,29 +1,29 @@
 import numpy as np
 
 X = np.array([[1,0,1], [0,1,1], [1,1,0]])
-
 YD = np.array([[1,0], [0,1], [1,1]])
-
 PESOS = np.array([[0.1,-0.5,-0.9], [0.6,0.2,-0.3]])
-
 UMBRAL = np.array([[0.5], [-0.8]])
+ERROR_LINEAL = []
 
 M = 3
 N = 2
 P = 3
-
 RA = 1
 ER = 0.1
 NI = 1000
+cont = 0
 
 for ENTRADA in X:
-    SALIDA = []
+    YR = []
+
     for PESO in PESOS:
         print(ENTRADA * PESO, "=", ENTRADA @ PESO)
-        SALIDA.append(ENTRADA @ PESO)
+        YR.append(ENTRADA @ PESO)
 
-    YR = SALIDA
     SALIDA = []
+    print("----------------------------------------")
+    print("YR = ",YR)
 
     for ESCALON in YR:
         if ESCALON >= 0:
@@ -31,10 +31,9 @@ for ENTRADA in X:
         else:
             SALIDA.append(0)
 
-    print(SALIDA)
-    print("******")
-    '''for YDa in YD:
-        print(YDa)
-        print(np.subtract(YDa, YR))
-    '''
-    print("-----")
+    print("Salida = ",SALIDA)
+    print("----------------------------------------")
+    np.subtract(YD[cont], YR)
+    
+    cont+=1
+    print("////////////////////////////////////////")
