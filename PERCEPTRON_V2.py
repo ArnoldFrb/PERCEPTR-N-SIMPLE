4ad49cc23c8268b9655ab0f9db9dcfbbd3ef6782
SLaxum <55930519+SLaxum@users.noreply.github.com>
2021-02-18 20:11:19 -0500

    Update PERCEPTRON_V2.py

--- PERCEPTRON_V2.py
+++ PERCEPTRON_V2.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 class Neurona:
+
     MATRIZ_ENTRADA = np.array([[1,0,1], [0,1,1], [1,1,0], [0,1,0]])
     MATRIZ_SALIDA = np.array([[1,0], [0,1], [1,1], [0,0]])
     MATRIZ_PESOS = np.array([[0.1,-0.5,-0.9], [0.6,0.2,-0.3]])
@@ -15,16 +16,26 @@ class Neurona:
     #METODO PARA ENTRENAR LA NEURONA
     def ENTRENAR():
 
+        print()
+        print("---ENTRENAMIENTO---")
+        print()
+
         #CICLO PARA ITERACIONES
         while True:
 
+            print("---------------------------")
+            print("ITERACION: ", Neurona.ITERACION_INICIAL+1)
+            print("---------------------------")
+
             ERROR_PATRON = []
 
+            #CICLO ENCARGADO DE RECORRER LOS PATRONES
             for I in range(len(Neurona.MATRIZ_ENTRADA)):
                 PATRON_PRESENTADO = []
-                for J in range(len(Neurona.MATRIZ_ENTRADA[0])):
-                    PATRON_PRESENTADO.append(Neurona.MATRIZ_ENTRADA[I][J])
+
+                PATRON_PRESENTADO.append(Neurona.MATRIZ_ENTRADA[I,:])
                 print(PATRON_PRESENTADO)
+
 
             Neurona.ITERACION_INICIAL+=1
 
