0f7cb038e823c9d20168b63b478ab3f3545fafbf
SLaxum <55930519+SLaxum@users.noreply.github.com>
2021-02-18 19:55:05 -0500

    METODO ENTRENAR LISTO

--- PERCEPTRON_V2.py
+++ PERCEPTRON_V2.py
@@ -1,18 +1,14 @@
 import numpy as np
 
 class Neurona:
-    MATRIZ_ENTRADA = np.array([[1,0,1], [0,1,1], [1,1,0]])
-    MATRIZ_SALIDA = np.array([[1,0], [0,1], [1,1]])
+    MATRIZ_ENTRADA = np.array([[1,0,1], [0,1,1], [1,1,0], [0,1,0]])
+    MATRIZ_SALIDA = np.array([[1,0], [0,1], [1,1], [0,0]])
     MATRIZ_PESOS = np.array([[0.1,-0.5,-0.9], [0.6,0.2,-0.3]])
     MATRIZ_UMBRALES = np.array([0.5, -0.8])
 
-    ENTRADA = 3
-    SALIDAS = 2
-    PATRONES = 3
-
     RATA_APRENDIZAJE = 1
     ERROR_ITERACION = 0.1
-    NUMERO_ITERACIONES = 1000
+    NUMERO_ITERACIONES = 1
 
     ITERACION_INICIAL = 0
 
@@ -24,10 +20,16 @@ class Neurona:
 
             ERROR_PATRON = []
 
+            for I in range(len(Neurona.MATRIZ_ENTRADA)):
+                PATRON_PRESENTADO = []
+                for J in range(len(Neurona.MATRIZ_ENTRADA[0])):
+                    PATRON_PRESENTADO.append(Neurona.MATRIZ_ENTRADA[I][J])
+                print(PATRON_PRESENTADO)
 
+            Neurona.ITERACION_INICIAL+=1
 
-            ITERACION_INICIAL+=1
-
-            #CONDICIONES DE PARADAS
-            if((ITERACION_INICIAL > NUMERO_ITERACIONES-1) or (ERMS <= ERROR_ITERACION)):
+            #CONDICIONES DE PARADA
+            if(Neurona.ITERACION_INICIAL > 0):
                 break
+
+Neurona.ENTRENAR()
