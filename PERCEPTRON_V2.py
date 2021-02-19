3556dea0b8ea04fb2335513f1d285c085cc3c857
SLaxum <55930519+SLaxum@users.noreply.github.com>
2021-02-18 22:32:23 -0500

    NEURONA CON METODOS OK

--- PERCEPTRON_V2.py
+++ PERCEPTRON_V2.py
@@ -2,12 +2,13 @@ import numpy as np
 
 class Neurona:
 
-    def __init__(self, RATA_APRENDIZAJE, ERROR_ITERACION, NUMERO_ITERACIONES):
+    #CONSTRUCTOR
+    def __init__(self, MATRIZ_ENTRADA = [[1,0,1], [0,1,1], [1,1,0]], MATRIZ_SALIDA = [[1,0], [0,1], [1,1]], MATRIZ_PESOS = [[0.1,-0.5,-0.9], [0.6,0.2,-0.3]], MATRIZ_UMBRALES = [0.5, -0.8], RATA_APRENDIZAJE = 1, ERROR_ITERACION = 0.1, NUMERO_ITERACIONES = 0):
         #MATRICES
-        self.MATRIZ_ENTRADA = np.array([[1,0,1], [0,1,1], [1,1,0], [0,1,0]])
-        self.MATRIZ_SALIDA = np.array([[1,0], [0,1], [1,1], [0,0]])
-        self.MATRIZ_PESOS = np.array([[0.1,-0.5,-0.9], [0.6,0.2,-0.3]])
-        self.MATRIZ_UMBRALES = np.array([0.5, -0.8])
+        self.MATRIZ_ENTRADA = np.array(MATRIZ_ENTRADA)
+        self.MATRIZ_SALIDA = np.array(MATRIZ_SALIDA)
+        self.MATRIZ_PESOS = np.array(MATRIZ_PESOS)
+        self.MATRIZ_UMBRALES = np.array(MATRIZ_UMBRALES)
         #VARIABLES DE CONFIGURACION
         self.RATA_APRENDIZAJE = RATA_APRENDIZAJE
         self.ERROR_ITERACION = ERROR_ITERACION
@@ -33,6 +34,8 @@ class Neurona:
             #CICLO ENCARGADO DE PRESENTAR LOS PATRONES
             for I in range(len(self.MATRIZ_ENTRADA)):
                 
+                print("PATRON: ", I+1)
+                print()
                 PATRON_PRESENTADO = (self.MATRIZ_ENTRADA[I,:])
                 print("PATRON PRESENTADO")
                 print(PATRON_PRESENTADO)
@@ -42,11 +45,99 @@ class Neurona:
                 print(SALIDA_PATRON)
                 print()
 
+                print("FUNCION SOMA")
+                print(self.FUNCION_SOMA(PATRON_PRESENTADO))
+                print()
+
+                print("FUNCION ESCALON")
+                print(self.FUNCION_ESCALON(self.FUNCION_SOMA(PATRON_PRESENTADO)))
+                print()
+
+                print("ERROR LINIAL")
+                print(self.FUNCION_ERROR_LINEAL(SALIDA_PATRON, self.FUNCION_ESCALON(self.FUNCION_SOMA(PATRON_PRESENTADO))))
+                print()
+
+                print("ERRORES PATRONES")
+                print((np.abs(self.FUNCION_ERROR_LINEAL(SALIDA_PATRON, self.FUNCION_ESCALON(self.FUNCION_SOMA(PATRON_PRESENTADO)))).sum()) / len(self.MATRIZ_SALIDA[0]))
+                print()
+                
+                ERROR_PATRON.append((np.abs(self.FUNCION_ERROR_LINEAL(SALIDA_PATRON, self.FUNCION_ESCALON(self.FUNCION_SOMA(PATRON_PRESENTADO)))).sum()) / len(self.MATRIZ_SALIDA[0]))
+
+                print("NUEVOS PESOS")
+                self.ACTUALIZAR_PESOS(PATRON_PRESENTADO, self.FUNCION_ERROR_LINEAL(SALIDA_PATRON, self.FUNCION_ESCALON(self.FUNCION_SOMA(PATRON_PRESENTADO))))
+                print(self.MATRIZ_PESOS)
+                print()
+
+                print("NUEVOS PESOS")
+                self.ACTUALIZAR_UMBRALES(self.FUNCION_ERROR_LINEAL(SALIDA_PATRON, self.FUNCION_ESCALON(self.FUNCION_SOMA(PATRON_PRESENTADO))))
+                print(self.MATRIZ_UMBRALES)
+                print()
+                print("--------------------")
+
+            print("--------------------")
+            #METODO PARA OBTENER EL ERROR DEL PATRON
+            print("ERRORES PATRONES")
+            print(ERROR_PATRON)
+            print()
+
+            #METODO PARA OBTENER EL ERROR DE LA ITERACION
+            print("ERROR DE LA ITERACION")
+            ERROR_RMS = (np.sum(ERROR_PATRON)) / len(self.MATRIZ_ENTRADA)
+            print(ERROR_RMS)
+            print()
+
             ITERACION_INICIAL+=1
 
             #CONDICIONES DE PARADA
-            if(ITERACION_INICIAL > 0):
+            if((ITERACION_INICIAL > self.NUMERO_ITERACIONES-1) or (ERROR_RMS <= self.ERROR_ITERACION)):
                 break
+        
+        print("NUMERO DE ITERACIONES REALIZADAS: ", ITERACION_INICIAL)
+        print()
 
-red = Neurona(1, 0.1, 0)
+    #METODO PARA OBTENER LA FUNCION SOMA
+    def FUNCION_SOMA(self, PATRON):
+        SL = []         #SALIDA DE LA FUNCION SOMA
+        for N in range(len(self.MATRIZ_PESOS)):
+            SLD = 0     #SUMATORIA DE LA FUNCION SOMA
+            for M in range(len(self.MATRIZ_PESOS[0])):
+                SLD += (PATRON[M] * self.MATRIZ_PESOS[N][M])
+            SL.append(SLD - self.MATRIZ_UMBRALES[N])
+        return SL
+
+    #METODO PARA OBTENER LA FUNCION ESCALON
+    def FUNCION_ESCALON(self, SALIDA_SOMA):
+        YR = []
+        for N in range(len(SALIDA_SOMA)):
+            YR.append(1 if SALIDA_SOMA[N]>=0 else 0)
+        return YR
+
+    #METODO PARA OBTENER EL ERROR LINEAL
+    def FUNCION_ERROR_LINEAL(self, SALIDA_PATRON, ESCALON):
+        EL = []          #ERROR LINEAL
+        for N in range(len(ESCALON)):
+            EL.append(SALIDA_PATRON[N] - ESCALON[N])
+        return EL
+    
+    #METODO PARA ACTUALIZAR PESOS
+    def ACTUALIZAR_PESOS(self, PATRON_PRESENTADO, ERROR_LINEAL):
+        for N in range(len(self.MATRIZ_PESOS)):
+            for M in range(len(self.MATRIZ_PESOS[0])):
+                self.MATRIZ_PESOS[N][M] = self.MATRIZ_PESOS[N][M] + (self.RATA_APRENDIZAJE * ERROR_LINEAL[N] * PATRON_PRESENTADO[M])
+
+    #METODO PARA ACTUALIZAR UMBRALES
+    def ACTUALIZAR_UMBRALES(self, ERROR_LINEAL):
+        for N in range(len(self.MATRIZ_UMBRALES)):
+            self.MATRIZ_UMBRALES[N] = self.MATRIZ_UMBRALES[N] + (self.RATA_APRENDIZAJE * ERROR_LINEAL[N] * 1)
+
+PRUEBA_MATRIZ_ENTRADA = [[1,0,1], [0,1,1], [1,1,0]]
+PRUEBA_MATRIZ_SALIDA = [[1,0], [0,1], [1,1]]
+PRUEBA_MATRIZ_PESOS = [[0.1,-0.5,-0.9], [0.6,0.2,-0.3]]
+PRUEBA_MATRIZ_UMBRALES = [0.5, -0.8]
+
+red = Neurona(
+    PRUEBA_MATRIZ_ENTRADA, PRUEBA_MATRIZ_SALIDA, 
+    PRUEBA_MATRIZ_PESOS, PRUEBA_MATRIZ_UMBRALES,
+    1, 0.01, 1000
+    )
 red.ENTRENAR()
