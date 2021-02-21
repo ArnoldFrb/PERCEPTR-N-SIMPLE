aed2649621cd5f7b17f10d076ee844ba6b2fa101
luisfersk <39956205+luisfersk@users.noreply.github.com>
2021-02-21 10:47:39 -0500

    modificaciones

--- PERCEPTRON_SIMPLE_01.py
+++ PERCEPTRON_SIMPLE_01.py
@@ -1,14 +1,23 @@
 import numpy as np
 
 class Neurona:
-    def __init__(self):
-        self.X = np.array([[1,0,1], [0,1,1], [1,1,0]])
-        self.YD = np.array([[1,0], [0,1], [1,1]])
-        self.PESOS = np.array([[0.1,-0.5,-0.9], [0.6,0.2,-0.3]])
-        self.UMBRAL = np.array([0.5, -0.8])
-        self.RATA = 1      #RATA DE APRENDIZAJE
-        self.ERROR = 0.1    #ERROR LINEAL
-        self.NUMERO_ITERACIONES = 3
+    def __init__(
+        self,
+        X = np.array([[1,0,1], [0,1,1], [1,1,0]]),
+        YD = np.array([[1,0], [0,1], [1,1]]),
+        PESOS = np.array([[0.1,-0.5,-0.9], [0.6,0.2,-0.3]]),
+        UMBRAL = np.array([0.5, -0.8]),
+        RATA = 1,
+        ERROR = 0.1,
+        NUMERO_ITERACIONES = 0
+        ):
+        self.X = X
+        self.YD = YD
+        self.PESOS = PESOS
+        self.UMBRAL = UMBRAL
+        self.RATA = RATA      #RATA DE APRENDIZAJE
+        self.ERROR = ERROR    #ERROR LINEAL
+        self.NUMERO_ITERACIONES = NUMERO_ITERACIONES
 
     def CALCULAR_YR(self, ENTRADA, PESOS, UMBRAL):
         YR = []
@@ -21,10 +30,8 @@ class Neurona:
     def FUNCION_ESCALON(self, YR):
         SALIDA = []
         for ESCALON in YR:
-            if ESCALON >= 0:
-                SALIDA.append(1)
-            else:
-                SALIDA.append(0)
+            YRa = 1 if ESCALON >= 0 else 0
+            SALIDA.append(YRa)
         return SALIDA
 
     def CALCULAR_ERROR_LINEAL(self, YD, SALIDA, CONTADOR):
@@ -54,11 +61,9 @@ class Neurona:
 
             ERROR_PATRON = []   #ERROR PATRON
             CONTADOR = 0
-
             print("---------------------------")
             print("ITERACION: ",ITERATE + 1)
             print("---------------------------")
-            
             #CICLO PARA ITERACIONES
             for ENTRADA in self.X:
                 print("PATRON: ",CONTADOR + 1,"\n")
@@ -81,14 +86,12 @@ class Neurona:
 
                 self.NEW_UMBRAL(self.UMBRAL, self.RATA, ERROR_LINEAL)
                 print("UMBRAL",self.UMBRAL,"\n")
-
                 print("---------------------------")
 
                 ERROR_RMS = self.CALCULAR_ERROR_RMS(ERROR_PATRON)
                 CONTADOR += 1
 
             ITERATE += 1
-
             print("---------------------------")
             print("ERROR DE LOS PATRONES",ERROR_PATRON,"\n")
             print("ERROR DE LA ITERACION",ERROR_RMS,"\n")
